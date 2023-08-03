4a8ed109cfbc725ecb806c1316e97d1a3dc799af
aakratigoel <aakratigoel@llokingbus.com>
2023-08-03 16:17:25 -0700

    debug

--- buglewebflowv9.js
+++ buglewebflowv9.js
@@ -147,10 +147,12 @@ $(document).ready(function () {
           $(".management_left").addClass("mobile_show_block");
           $(".management_right").addClass("hide_block");
           $(".management_right").removeClass("mobile_show_block");
+          $(".management_right").removeClass("show_right_block");
         } else if (mobile_content1count == 2) {
           managementSection();
           $(".management_left").addClass("hide_block");
           $(".management_left").removeClass("mobile_show_block");
+          $(".management_left").removeClass("show_left_block");
           $(".management_right").addClass("mobile_show_block");
         }
       } else if (content1count == 2) {
