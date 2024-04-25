4aa9ca120814b2e1163343a7c52cfda65f3f9017
aakratigoel <aakratigoel@llokingbus.com>
2024-04-24 21:45:42 -0700

    fade in fade out update

--- buglewebflowv97.js
+++ buglewebflowv97.js
@@ -12,9 +12,16 @@ $(document).ready(function () {
     $(".register_feature_header").removeClass("active");
     $(".event_feature_header").removeClass("active");
     $(".shift_feature_header").removeClass("active");
-    $(".event_registration_image").fadeOut(2000);
-
-    $(".enterprise_account").fadeIn(2000);
+    // $(".event_registration_image").fadeOut(2000);
+    // $(".enterprise_account").fadeIn(2000);
+    $(".event_registration_image").fadeOut(function () {
+      // This function will be called once the fadeOut animation is complete
+      // Use setTimeout to add a delay before fading in the next element
+      setTimeout(function () {
+        // Fade in the second element with class "element-to-fade-in" after a delay of 1000 milliseconds (1 second)
+        $(".enterprise_account").fadeIn();
+      }, 1000); // Adjust the delay as needed (in milliseconds)
+    });
   });
   $(".register_feature_header").on("click", function () {
     $(this).addClass("active");
